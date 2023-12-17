a3736400ed5ce2d8852f207e5b7f09cd8a387932
francistobi <frobigwe305@gmail.com>
2023-12-17 13:37:10 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 const firstname = document.getElementById("firstName").value;
 const lastname = document.getElementById("lastName").value;
-const password = document.getElementById("password")
+const password = document.getElementById("password");
 const showBtn = document.querySelector(".showimg");
 const passErr = document.querySelector(".passworderr");
 const firstNameErr = document.querySelector(".firstnamerr");
-let passworVal = password.value;
+// let passworVal = password.value;
 
 showBtn.addEventListener("click", () => {
-  password.type = (password.type === "password") ? "text" : "password";
-});//password visibility with turnary operator
-
+  password.type = password.type === "password" ? "text" : "password";
+}); //password visibility with turnary operator
 
 document.getElementById("regform").addEventListener("submit", function (event) {
   event.preventDefault();
+  const passworVal = document.getElementById("password").value;
   validatepassword(passworVal);
+  console.log(passworVal);
 });
+
 function validatepassword(passworVal) {
   let regExp = /[.,!#?\\-]/;
-  while(passworVal.length < 8) {
+  if (passworVal.length < 8) {
     passErr.textContent = "Password must be greater than 8 characters";
     return false;
-  }
-  if (!passworVal.match(regExp)) {
+  } else if (!passworVal.match(regExp)) {
     passErr.textContent = "Password must contain (., !, #, ?, or -)";
     return false;
   } else {
@@ -29,5 +30,3 @@ function validatepassword(passworVal) {
     return true;
   }
 } //validate the password by checking if it contains some special character
-
-  
