e486e7f63f8285025769675aef4af5e262a70778
francistobi <frobigwe305@gmail.com>
2023-12-19 14:32:56 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const password = document.getElementById("password");
 const showBtn = document.querySelector(".showimg");
 const passErr = document.querySelector(".passworderr");
 const firstNameErr = document.querySelector(".firstnamerr");
-// let passworVal = password.value;
+//selectring element from the dom to manipulate
 
 showBtn.addEventListener("click", () => {
   password.type = password.type === "password" ? "text" : "password";
@@ -14,9 +14,11 @@ document.getElementById("regform").addEventListener("submit", function (event) {
   validatepassword(passworVal);
   validateFirstName();
 });
+//event listener that listens and handle the submittion of the form
 
 function validatepassword(passworVal) {
   let regExp = /[.,!#?\\-]/;
+  //regular expression for the first letter all special character
   if (passworVal.length < 8) {
     passErr.textContent = "Password must be greater than 8 characters";
     return false;
@@ -27,11 +29,12 @@ function validatepassword(passworVal) {
     passErr.textContent = "";
     return true;
   }
-} //validate the password by checking if it contains some special character
+} //validate the password by checking if it contains some special character and if length is less than 8 character
 
 function validateFirstName() {
   const firstName = document.getElementById("firstName").value;
   let firReg = /^[A-Z]/;
+  //regular expression for the first letter that start with capital letter 
   if (!firstName.match(firReg)) {
     firstNameErr.textContent = "Must start capital letter";
     return false;
@@ -39,4 +42,6 @@ function validateFirstName() {
     firstNameErr.textContent = "";
     return true;
   }
+  //check if the first letter of the first name does not match the regular expression
 }
+//validate the firstname by checking if it starts with capital letter 
